fd8ae58c1456cb1eb05a6e032a2cfeed7ac7bb46
Dmitriy Mukhin <dmitriy.mukhin@gmail.com>
2017-05-02 23:42:04 -0400

    added "Install Printer" function; refactored

---
+++ PrnCap/TcpReceiver.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrnCap
+{
+    using System.IO;
+    using System.Net;
+    using System.Net.Sockets;
+    using System.Threading;
+    class TcpReceiver
+    {
+        Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        ManualResetEvent handlerObtained = new ManualResetEvent(false);
+        string destPath = Path.Combine(Path.GetTempPath(), "PrnCap");
+        public void Listening(EndPoint endPoint, int backlog = 100)
+        {
+            // Bind the socket to the local endpoint and listen for incoming connections.  
+            try
+            {
+                listener.Bind(endPoint);
+                listener.Listen(backlog);
+
+                while (true)
+                {
+                    // Set the event to nonsignaled state.  
+                    handlerObtained.Reset();
+
+                    // Start an asynchronous socket to listen for connections.
+                    Console.WriteLine(NowWithTimezone().ToString("o") + "\tListening...");
+                    listener.BeginAccept(AcceptCallback, null);
+
+                    // Wait until a connection is made before continuing.  
+                    handlerObtained.WaitOne();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        private static DateTimeOffset NowWithTimezone()
+        {
+            DateTime _now = DateTime.Now;
+            DateTimeOffset _now_w_tz = new DateTimeOffset(_now, TimeZoneInfo.Local.GetUtcOffset(_now));
+            return _now_w_tz;
+        }
+
+        void AcceptCallback(IAsyncResult ar)
+        {
+            // Get the socket that handles the client request.  
+            Socket handler = listener.EndAccept(ar);
+
+            // Signal the main thread to continue.  
+            handlerObtained.Set();
+
+            // Create the state object.  
+            StateObject state = new StateObject() { workSocket = handler };
+            handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0, ReadCallback, state);
+        }
+        // State object for reading client data asynchronously  
+        class StateObject
+        {
+            // Client  socket.  
+            public Socket workSocket = null;
+            // Size of receive buffer.  
+            public const int BufferSize = 1024;
+            // Receive buffer.  
+            public byte[] buffer = new byte[BufferSize];
+            // Received data. 
+            public MemoryStream data = new MemoryStream();
+        }
+        void ReadCallback(IAsyncResult ar)
+        {
+            String content = String.Empty;
+
+            // Retrieve the state object and the handler socket  
+            // from the asynchronous state object.  
+            StateObject state = (StateObject)ar.AsyncState;
+            Socket handler = state.workSocket;
+
+            // Read data from the client socket.   
+            int bytesRead = handler.EndReceive(ar);
+
+            if (bytesRead > 0)
+            {
+                // There  might be more data, so store the data received so far.  
+                state.data.Write(state.buffer, 0, bytesRead);
+
+                // Not all data received. Get more.  
+                handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0, ReadCallback, state);
+            }
+            else
+            {
+                byte[] _data = state.data/*.GetBuffer()*/.ToArray();
+                //Console.Write(Encoding.UTF8.GetString(_data));
+                Directory.CreateDirectory(destPath);
+                File.WriteAllBytes(Path.Combine(destPath, NowWithTimezone().ToString("yyyy-MM-dd_HH-mm-ss.fffzzz").Replace(":", "")), _data);
+            }
+        }
+    }
+}
---
+++ PrnCap/WmiWrapper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrnCap
+{
+    using System.Management;
+    class WmiWrapper
+    {
+        static IEnumerable<ManagementObject> Query(string className)
+        {
+            return new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM " + className)
+                .Get().Cast<ManagementObject>();
+        }
+        static ManagementPath Create(string className, params object[] property)
+        {
+            var _class = new ManagementClass("root\\cimv2", className, new ObjectGetOptions());
+            _class.Get();
+            var _obj = _class.CreateInstance();
+            for (int i = 0; i < property.Length - 1; i += 2)
+                _obj.SetPropertyValue("" + property[i], property[i + 1]);
+            return _obj.Put();
+        }
+        const string PRINTER_NAME = "iFexo localhost printer";
+        const string PORT_NAME = "iFexo localhost printer port";
+        public static void InstallPrinter()
+        {
+            var _printer = Query("Win32_Printer").FirstOrDefault(o => PRINTER_NAME.Equals(o["Name"]) && PRINTER_NAME.Equals(o["DeviceID"]));
+            if (_printer == null)
+            {
+                var _port = Query("Win32_TCPIPPrinterPort").FirstOrDefault(o => PORT_NAME.Equals(o["Name"]));
+                if (_port == null)
+                {
+                    Create("Win32_TCPIPPrinterPort",
+                        "Name", PORT_NAME,
+                        "Protocol", 1,
+                        "HostAddress", "127.0.0.1",
+                        "PortNumber", 9100,
+                        "SNMPEnabled", false);
+                }
+                Create("Win32_Printer",
+                    "Name", PRINTER_NAME,
+                    "DeviceID", PRINTER_NAME,
+                    "DriverName", "Generic / Text Only",
+                    "PortName", PORT_NAME,
+                    "Network", true,
+                    "Shared", false);
+            }
+        }
+    }
+}
